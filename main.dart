97177f7baa5aacb198a982fde5d6a0166c4845c1
Ahmed1Rabie <89657667+Ahmed1Rabie@users.noreply.github.com>
2023-03-26 23:27:21 +0200

    Assignment 3

---
+++ main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      // A widget which will be started on application startup
+      home: MyHomePage(title: 'First Application'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text(
+          'Ahmed Rabie',
+        ),
+      ),
+    );
+  }
+}
